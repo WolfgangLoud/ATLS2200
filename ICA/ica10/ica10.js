d24794adc79cb48396f05bb3c3901c66e08b84d4
WolfgangLoud <wolo8873@colorado.edu>
2023-03-21 20:38:42 -0600

    ICA10 complete/ index update

---
+++ ICA/ica10/ica10.js
@@ -0,0 +1,7 @@
+// function to display alert after 5 seconds
+function displayAlert() {
+    alert("You've been on this page for 5 seconds!");
+  }
+
+  // call the function after 5 seconds
+  setTimeout(displayAlert, 5000);
