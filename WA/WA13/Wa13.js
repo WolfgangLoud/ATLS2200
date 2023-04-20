20f76ddf9b2c624cc62e6cfb3e7e5ad6c2826dcc
WolfgangLoud <wolo8873@colorado.edu>
2023-04-20 13:23:46 -0600

    WA13 complete

--- WA/WA13/Wa13.js
+++ WA/WA13/Wa13.js
@@ -41,30 +41,71 @@ const output = document.querySelector('.output');
 let outputInt = parseInt(output.textContent);
 console.log(outputInt);
 
-const minusButton = document.querySelector('.minus-button').addEventListener('click', minus);
-const plusButton = document.querySelector('.plus-button').addEventListener('click', plus);
-const resetButton = document.querySelector('.reset-button').addEventListener('click', reset);
-const randomButton = document.querySelector('.random-button').addEventListener('click', random);
-const submitButton = document.querySelector('.submit-button').addEventListener('click', submit);
-
-
-/* const button = document.querySelector('.button');
-const output = document.querySelector('.output');
-let phone_content = document.querySelector('.phone');
-button.addEventListener('click', updateOutput);
-function updateOutput() {
-    output.textContent = phone_content.value;
-    alert(phone_content.value);
-}
-*/
-
 
 var slider = document.getElementById("myRange");
-var sliderSubmit = document.querySelector(".slider-submit-button").addEventListener('click', update);
 var sliderOutput = document.querySelector(".slider-output");
+const Output = document.getElementById(".output");
+const incrementButton = document.getElementById("increment-button");
+const decrementButton = document.getElementById("decrement-button");
 
+
+let incrementTimer;
+let decrementTimer;
+let incrementVelocity = 1;
+let decrementVelocity = 1;
 
 // Update the current slider value (each time you drag the slider handle)
+
 function update() {
-  sliderOutput.textContent = slider.value;
+  sliderOutput.textContent = "Volume: " + slider.value;
 }
+
+slider.addEventListener("input", () => {
+  output.innerHTML = "Volume: " + slider.value;
+});
+
+// Generate and set random value when slider is clicked
+slider.addEventListener("input", () => {
+    const randomValue = Math.floor(Math.random() * 101);
+    slider.value = randomValue;
+    output.innerHTML = "Volume: " + randomValue;
+  });
+  
+
+ // Increase increment velocity and update slider value and output element
+function increment() {
+    slider.value = parseInt(slider.value) + incrementVelocity;
+    output.innerHTML = "Volume: " + slider.value;
+    incrementVelocity++;
+    incrementTimer = setTimeout(increment, 50);
+    }
+
+// Increase decrement velocity and update slider value and output element
+function decrement() {
+    slider.value = parseInt(slider.value) - decrementVelocity;
+    output.innerHTML = "Volume: " + slider.value;
+    decrementVelocity++;
+    decrementTimer = setTimeout(decrement, 50);
+  }
+
+// Add event listeners to increment and decrement buttons
+incrementButton.addEventListener("mousedown", () => {
+    incrementVelocity = 1;
+    increment();
+    });
+
+    decrementButton.addEventListener("mousedown", () => {
+        decrementVelocity = 1;
+        decrement();
+      });
+
+      // Stop timer when space bar is pressed
+         // Stop timer when space bar is pressed
+         window.addEventListener("keydown", (event) => {
+            if (event.code === "Space") {
+              clearTimeout(incrementTimer);
+              clearTimeout(decrementTimer);
+              incrementVelocity = 1;
+              decrementVelocity = 1;
+            }
+          });
